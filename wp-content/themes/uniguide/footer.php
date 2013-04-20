55cab36cf5c07409378f47966a052fd989887caa
Vincent Leung <vincentleung1991@gmail.com>
2013-04-20 17:55:56 +0200

    first try on index.php

---
+++ wp-content/themes/uniguide/footer.php
@@ -0,0 +1,22 @@
+			<footer>
+				<div id="footer-links">
+					<div>
+						<h3>Imprint</h3>
+					</div>		
+					<div>
+						<h3>Disclaimer</h3>
+					</div>	
+					<div>
+						<h3>Contact</h3>
+					</div>	
+					<div>
+						<h3>Help</h3>
+					</div>	
+				</div>
+				<div id="copyright">
+					<p>&copy Copyright 2013. All rights reserved</p>
+                          </footer>
+			  </section>			
+		</section>           
+	</body>
+</html>
---
+++ wp-content/themes/uniguide/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<?php get_header(); ?>
+		<section id="container">
+			<section id="content">
+				<section id="left" class="column">
+					<a href="#">menu</a>
+					<a href="#">About Us</a>
+					<a href="#">People</a>
+					<a href="#">Research</a>
+					<a href="#">Studies</a>
+					<a href="#">Training</a>
+					<a href="#">Campus Info</a>
+				</section>
+				<section id="main" class="column">
+					<h1 id="main-title"><a href="#">ETH Guide</a></h1>
+					<section id="headline">
+						<article id="headline-main">
+							<img id="headline-img" src="images/teaser-article.jpg" alt=""/>
+							<div id="headline-text">
+								<h3>Shedding light on the earthquake situation.</h3>
+								<p>Researchers from the Swiss Seismological Service have worked together with the Seismology and Geodynamics group at ETH Zurich and with local support in Bhutan to install a temporary seismological network. They plan to use the network to close a gap in geophysical research of the Himalayas. <a href="#">Read more...</a></p>
+							</div>
+						</article>
+						<div id="headline-footer">
+							<a href="#">Sept 21  by David</a>
+							<span>000 views</span>
+						</div>
+					</section>
+					<section class="row-container">
+						<section class="row">
+							<article class="news">
+								<h3>Subordinate animals as guinea pigs.</h3>
+								<a href="#"><img src="images/article-1.jpg" alt=""/></a>
+								<p>Subordinate animals must face higher risks than dominant ones Dominant meerkat females yield to their subaltern group members when faced with a dangerous obstacle: as a group of these animals reaches a road.</p>
+							</article>
+							<article class="news">
+								<h3>EU launches two flagships.</h3>
+								<a href="#"><img src="images/article-5.jpg" alt=""/></a>
+								<p>The EU is launching two large-scale research projects for the next ten years in the form of “Graphene” and the “Human Brain Project”. “FuturICT” and “Guardian Angels”.</p>
+							</article>
+						</section>
+					</section>
+					<section class="row-container">
+						<section class="row">
+							<article class="news">
+								<h3>Research success by the dozen.</h3>
+								<a href="#"><img src="images/article-3.jpg" alt=""/></a>
+								<p>Thanks to twelve outstanding professors, over 33 million Swiss francs from the European Research Council (ERC) will flow into ETH Zurich.</p>
+							</article>
+							<article class="news">
+								<h3>Research success by the dozen - 2.</h3>
+								<a href="#"><img src="images/article-4.jpg" alt=""/></a>
+								<p>The EU is launching two large-scale research projects for the next ten years in the form of “Graphene” and the “Human Brain Project”.</p>
+							</article>
+						</section>
+					</section>
+					<section class="row-container">
+						<section class="row">
+							<article class="news">
+								<h3>How graphene behaves on a small scale.</h3>
+								<a href="#"><img src="images/article-2.jpg" alt=""/></a>
+								<p>ETH Zurich is involved in the “Graphene Flagship” project through Klaus Ensslin, a professor of experimental physics. ETH Life spoke to him about the advantages of graphene and his personal vision.</p>
+							</article>
+							<article class="news">
+								<h3>Competitively displaced native species do suffer extinctions.</h3>
+								<a href="#"><img src="images/article-6.jpg" alt=""/></a>
+								<p>ETH-Ecology-Professor Jonathan Levine showed that invasive species can indeed force the extinction of their native competitors over a long period of time.</p>
+							</article>
+						</section>
+						<div id="load-more"><a href="#">Load more...</a></div>
+					</section>
+				</section>
+				<section id="right" class="column">
+					<header>
+						<h3 id="right-title">Photo Tour</h3>
+					</header>
+					<div class="photo-group">
+						<h3>ETH Zentrum</h3>
+						<div>
+							<a href="#"><img src="images/eth-1.jpg"></a>
+							<a href="#"><img src="images/eth-2.jpg"></a>
+						</div>
+						<div>							
+							<a href="#"><img src="images/eth-3.jpg"></a>
+							<a href="#"><img src="images/eth-4.jpg"></a>							
+						</div>
+					</div>
+					<div class="photo-group">
+						<h3>ETH Hoenggerberg</h3>
+						<div>
+							<a href="#"><img src="images/eth-5.jpg"></a>
+							<a href="#"><img src="images/eth-6.jpg"></a>
+						</div>
+						<div>
+							<a href="#"><img src="images/eth-7.jpg"></a>
+							<a href="#"><img src="images/eth-8.jpg"></a>							
+						</div>
+					</div>
+					<div id="stay-informed">
+						<h2>Stay Informed:</h2>
+						<img id="fbk-icon" src="images/fbk-icon.gif">
+						<a href="http://facebook.com/">Facebook</a>
+						<img id="twt-icon" src="images/twt-icon.gif">
+						<a href="http://twitter.com/">Twitter</a>
+					</div>
+				</section>
+				<div>&nbsp;</div>
+			</div>
+<?php get_footer() ?>
