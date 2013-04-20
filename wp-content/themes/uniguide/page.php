b2ec19e1f1e270730dae19b68796084bab3bcc0c
Vincent Leung <vincentleung1991@gmail.com>
2013-04-20 19:25:30 +0200

    fixed top nav

--- wp-content/themes/uniguide/page.php
+++ wp-content/themes/uniguide/page.php
@@ -2,6 +2,7 @@
 <?php get_sidebar(); ?>
 	<section id="main" class="column">
 		<?php while( have_posts() ) : the_post(); ?>
+			<h1 id="main-title"><a href="#"><?php the_title()?></a></h1>
 			<?php the_content(); ?>
 		<?php endwhile; ?>
 	</section>
