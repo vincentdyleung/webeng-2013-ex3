7b21667d3240175590849b40b72636ad24c42fcd
Vincent Leung <vincentleung1991@gmail.com>
2013-04-24 01:24:55 +0200

    using bloginfo to display title

--- wp-content/themes/uniguide/header.php
+++ wp-content/themes/uniguide/header.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<title>ETH Guide</title>
+		<title><?php echo bloginfo('name'); ?></title>
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>" />
         <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
 		<script type="text/javascript" src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
